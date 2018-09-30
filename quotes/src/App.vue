3b0f8feb3759fba5bd5aeb9bec06dbf0fa498f22
danmaina <mainadanarin@gmail.com>
2018-09-30 15:32:12 +0300

    initial commit

---
+++ quotes/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div>
+            <h1>Quotes</h1>
+        </div>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+export default {
+  data: function() {
+    return {
+      maxQuotes: 10,
+      quotes: [
+        "We Do not rise to the level of our expectations, we fall to the level of our training."
+      ]
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ quotes/src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <div class="container-fluid">
+            <form action="">
+                <div class="col-md-8 col-md-offset-1">
+                    <textarea v-model="quote" required class="form-control" 
+                        id="" cols="30" rows="10" placeholder="Create New Quote" @keyup.enter.prevent="createQuote"></textarea>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createQuote() {
+      console.log("quote added: " + this.quote);
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
