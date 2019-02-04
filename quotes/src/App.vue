d836c71fc9b6f17327c21bf89b80ba88639be166
TAHA BEN AMMAR <tahainfo@yahoo.fr>
2019-02-04 17:44:40 +0100

    changing default quote title

--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="row">
     <div class="container">
-        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-        <new-quote @quoteAdded="createNewQuote"></new-quote>
-        <hr>
-        <app-quote-grid :quotes="quotes" @quoteToDelete="deleteQuote"></app-quote-grid>
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+      <new-quote @quoteAdded="createNewQuote"></new-quote>
+      <hr>
+      <app-quote-grid :quotes="quotes" @quoteToDelete="deleteQuote"></app-quote-grid>
     </div>
   </div>
 </template>
@@ -14,11 +14,11 @@ import QuoteGrid from "./components/QuoteGrid.vue";
 import NewQuote from "./components/NewQuote.vue";
 import AppHeader from "./components/Header.vue";
 export default {
-  data: function() {
+  data: function () {
     return {
       maxQuotes: 10,
       quotes: [
-        "We Do not rise to the level of our expectations, we fall to the level of our training."
+        "Git proud : We Do not rise to the level of our expectations, we fall to the level of our training."
       ]
     };
   },
@@ -28,16 +28,16 @@ export default {
     appQuoteGrid: QuoteGrid
   },
   methods: {
-    createNewQuote(quote) {
+    createNewQuote (quote) {
       if (this.quotes.length >= this.maxQuotes) {
         return alert(
           "You have reached the maximum number of quotes allowed: " +
-            this.maxQuotes
+          this.maxQuotes
         );
       }
       this.quotes.push(quote);
     },
-    deleteQuote(index) {
+    deleteQuote (index) {
       this.quotes.splice(index, 1);
     }
   }
